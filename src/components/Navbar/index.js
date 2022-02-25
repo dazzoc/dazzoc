157b14e92514646fe287d246ee7aaa4e3ed8a0a6
Christopher Dazzo <christopherdazzo@Christophers-MacBook-Air.local>
2022-02-25 09:46:01 -0600

    wip: nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,24 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
 
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">Dazzo</NavLogo>
+                <MobileIcon>
+                    <MenuIcon />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to='about'>About</NavLinks>
+                    </NavItem>
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+    </>
   )
 }
 
