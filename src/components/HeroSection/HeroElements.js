02355df7b94b288643ca4a7ad53cba4d01e5907e
Christopher Dazzo <dazzo.chris@gmail.com>
2022-04-01 09:07:02 -0500

    wip: hero-section

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -30,7 +30,7 @@ export const HeroImgWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 500px;
+    width: 600px;
 `;
 
 export const Circle = styled.img`
@@ -40,7 +40,44 @@ export const Circle = styled.img`
 export const HeroContent = styled.div`
     z-index: 3;
     max-width: 1100px;
+    margin-top: 20px;
     display: flex;
     justify-content: center;
     align-items: center;
 `;
+
+export const CircleBox = styled.img`
+    height: 150px;
+    margin-right: 20px;
+`;
+
+export const Name = styled.div` 
+    margin-right: 60px;
+`;
+
+export const NameText = styled.h1`
+    font-size: 3.5rem;
+    font-weight: 400;
+    transform: scaleY(1.4);
+    margin-bottom: 10px;
+`;
+
+export const TextWrap = styled.div`
+    height: 150px;
+`;
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 15px;
+`;
+
+export const Arrow1 = styled.img`
+    height: 20px;
+    margin-right: 10px;
+`;
+
+export const Text = styled.h1`
+    font-size: 1.2rem;
+`
--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,11 +1,13 @@
-import w from '../../images/w-01.svg';
-import e from '../../images/e-01.svg';
-import l from '../../images/l-01.svg';
-import c from '../../images/c-01.svg';
-import o from '../../images/o-01.svg';
-import m from '../../images/m-01.svg';
+// import w from '../../images/w-01.svg';
+// import e from '../../images/e-01.svg';
+// import l from '../../images/l-01.svg';
+// import c from '../../images/c-01.svg';
+// import o from '../../images/o-01.svg';
+// import m from '../../images/m-01.svg';
 import CircleGuy from '../../images/CircleGuy.jpg';
-import { HeroContainer, HeroBg, HeroContent, HeroImgWrapper, Circle } from './HeroElements';
+import CircleBoxGuy from '../../images/CircleGuyBoxed-01.svg';
+import ArrowRight1 from '../../images/ArrowRight-01.svg';
+import { HeroContainer, HeroBg, HeroContent, HeroImgWrapper, Circle, CircleBox, Name, NameText, TextWrap, Row, Arrow1, Text } from './HeroElements';
 
 function HeroSection() {
   return (
@@ -22,7 +24,27 @@ function HeroSection() {
                 <img src={o} alt="o" className='welcome' id='o'/>
                 <img src={m} alt="m" className='welcome' id='m'/>
                 <img src={e} alt="e" className='welcome' id='e2'/> */}
-              
+              <Row>
+                <CircleBox src={CircleBoxGuy} alt='CircleBox'/>
+                <Name>
+                  <NameText>CHRIS</NameText>
+                  <NameText>DAZZO</NameText>
+                </Name>
+              </Row>
+              <TextWrap>
+                <Row>
+                  <Arrow1 src={ArrowRight1} alt='arrow'/>
+                  <Text>WEB DEVELOPMENT</Text>
+                </Row>
+                <Row>
+                  <Arrow1 src={ArrowRight1} alt='arrow'/>
+                  <Text>ILLUSTRATION</Text>
+                </Row>
+                <Row>
+                  <Arrow1 src={ArrowRight1} alt='arrow'/>
+                  <Text>DESIGN</Text>
+                </Row>
+              </TextWrap>
             </HeroContent>
         </HeroBg>
     </HeroContainer>
